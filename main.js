e3c83ceba9a7e211823f7d0cc1445a66d798aa8d
ahmed-hessen <ahmedhessen818@gmail.com>
2023-08-06 00:30:29 +0300

    creat projects

---
+++ main.js
@@ -0,0 +1,174 @@
+// Start  Loading Page 
+window.addEventListener("load",function (){
+    let loader = document.querySelector(".loader")
+    loader.classList.add("loader-hidden")
+
+    loader.addEventListener("transitionend",function(){
+        document.body.removeChild("loader");
+    })
+})
+// End  Loading Page 
+// ==================================
+//Start nav bar hover on account 
+let account = document.getElementById("account");
+let accountMenu = document.querySelector(".menu");
+
+account.addEventListener("mouseenter",function(){
+    accountMenu.style.display="block";
+
+})
+account.addEventListener("mouseleave",function(){
+    accountMenu.style.display="none";
+})
+accountMenu.addEventListener("mouseenter",function(){
+    accountMenu.style.display="block";
+})
+accountMenu.addEventListener("mouseleave",function(){
+    accountMenu.style.display="none";
+})
+//End nav bar hover on account 
+// =======================================================
+// Start  Nav Bar Click On Categories 
+let categories = document.getElementById("category");
+let categoryMenu=document.querySelector(".category-menu");
+
+categories.addEventListener("click",()=> {
+  body.classList.toggle("active")
+})
+
+// End  Nav Bar Click On Categories
+// =========================================================== 
+// Start Grab Product Section 
+const productsContainer = document.querySelector(".products-container");
+let isDragging = false;
+let startPos = 0;
+let scrollLeft = 0;
+
+productsContainer.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    startPos = e.clientX;
+    scrollLeft = productsContainer.scrollLeft;
+    productsContainer.style.cursor = "grabbing";
+
+    // Prevent text selection during drag
+    e.preventDefault();
+});
+
+document.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+    const delta = e.clientX - startPos;
+    productsContainer.scrollLeft = scrollLeft - delta;
+});
+
+document.addEventListener("mouseup", () => {
+    isDragging = false;
+    productsContainer.style.cursor = "grab";
+});
+
+document.addEventListener("mouseleave", () => {
+    isDragging = false;
+    productsContainer.style.cursor = "grab";
+});
+// End Grab Product Section 
+// ========================================================
+// Start Top Selling Section 
+const carouselContent = document.querySelector('.carousel-content');
+let scrollAmount = 250; 
+let scrollPosition = 0;
+
+function scrollCarousel(scrollOffset) {
+  scrollPosition += scrollOffset;
+  if (scrollPosition < 0) {
+    scrollPosition = 0;
+  } else if (scrollPosition > carouselContent.scrollWidth - carouselContent.clientWidth) {
+    scrollPosition = carouselContent.scrollWidth - carouselContent.clientWidth;
+  }
+  carouselContent.style.transform = `translateX(-${scrollPosition}px)`;
+}
+
+document.querySelector('.prev').addEventListener('click', () => {
+  scrollCarousel(-scrollAmount);
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+  scrollCarousel(scrollAmount);
+});
+// End Top Selling 
+// =======================================
+// Start best Seller JS 
+let switcherButtons = document.querySelectorAll(".icons li");
+let allCards = document.querySelectorAll(".carousel-content .card");
+
+switcherButtons.forEach((li) => {
+  li.addEventListener("click", function removeActiveAndManageImages(){
+      // remove class active and add it on one we click
+      switcherButtons.forEach((li) => {
+          li.classList.remove("active");
+          this.classList.add("active");
+      })   
+      // disappear all cards 
+      allCards.forEach((card)=> {
+        card.style.display = "none";
+    })  
+    // appear all cards 
+      document.querySelectorAll(this.dataset.cat).forEach((card) =>{
+        card.style.display="block";
+      })
+      document.getElementById("best-image").style.marginLeft="200px"
+  })
+})
+// End best Seller JS
+// ==========================================
+// Start  Add To Cart Section 
+let openShopingCart = document.querySelector(".cart");
+let body = document.querySelector("body");
+
+openShopingCart.addEventListener("click",()=>{
+  body.classList.toggle("activation");
+})
+
+let productsList= [] 
+
+ // Function to handle adding a card to the cart
+ function addToCart(button) {
+  const cardDiv = button.closest('.card');
+
+  // Extract the specific values from the card
+  const name = cardDiv.querySelector('.card-text').textContent;
+  const pictureSrc = cardDiv.querySelector('.card-img-top').src;
+  const price = parseFloat(cardDiv.querySelector('.sar-price span').textContent);
+
+  let product = 
+    {
+      name,
+      pictureSrc,
+      price}
+  productsList.push(product)
+  // Create cart item elements
+  const cartItem = document.createElement('li');
+  productsList.forEach(function(product) {
+    cartItem.innerHTML = `
+    <ul class="listcards d-flex justify-content-between gap-3 px-5" style="list-style: none; color: black;">
+    <li style="font-size: 12px; flex:2;">${product.name}</li>
+        <li style="flex:2"><img src="${product.pictureSrc}" alt="Product Image" style="width: 60px; height: auto;"></li>
+        <li style="flex:1"> ${product.price.toFixed(2)}</li>
+    </ul>
+`;
+});
+    // increase the quantity of cart by one every time 
+  const quantityElement = document.getElementById('quantity');
+  const currentQuantity = parseInt(quantityElement.textContent);// to change it into number
+  quantityElement.textContent = currentQuantity + 1; // add one every time i add to cart 
+
+  // calculate the total price of the element in the cart 
+  const totalPriceElement = document.querySelector('.total');
+  const currentTotal = parseFloat(totalPriceElement.textContent);
+  const newTotal = currentTotal + price;
+  totalPriceElement.textContent = newTotal.toFixed(2)+" SAR";
+
+  // Append the cart item to the cart section
+  const cartItemsList = document.querySelector('.cart-items');
+  cartItemsList.appendChild(cartItem);
+}
+// End  Add To Cart Section 
+
---
+++ sign.js
@@ -0,0 +1,48 @@
+
+// Start function to Show The Password
+let showPassWord=document.getElementById("show");
+let pass = document.getElementById("pass");
+
+showPassWord.onclick = function showMyPass() {
+    if (pass.type =="password" && pass.value!== ""){
+            pass.type="text";
+            showPassWord.textContent="Hide Password";
+    }else {
+        pass.type="password";
+        showPassWord.textContent="Show Password";
+    }
+}
+// End function to Show The Password
+// Start function to Validate 
+function formValidate(){
+let myEmail = document.getElementById("email").value;
+let passWord = document.getElementById("pass").value;
+let myName= document.getElementById("name").value;
+
+let error = document.getElementById("error");
+let text="";
+    if(myName == "" || myName.length < 3){
+        text="Please Enter Avalid Name"
+        error.innerHTML=text;
+        return false
+}else if (myEmail.length < 10 || myEmail.indexOf("@")== -1 ){
+    text="Please Enter Avalid Email"
+    error.innerHTML=text;
+    return false;
+}else if (passWord.length < 8 ){
+    text="Please enter more than eight character in password"
+    error.innerHTML=text;
+    return false;
+}else {
+    return true;
+};
+}
+// End function to Validate 
+
+
+
+
+
+
+
+
